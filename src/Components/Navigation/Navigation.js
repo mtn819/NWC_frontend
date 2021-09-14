289d973aa194f17273bfde64fd47b6f06971bcd9
Mikonuji <mtneugebauer1998@gmail.com>
2021-09-14 14:15:54 -0500

    fixed nav

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -12,12 +12,13 @@ function Navigation() {
     <div className="navigation">
         <div></div>
         <Link className="navigation_link" to="/">HOME</Link>
-        <Link className="navigation_link" to="/About">ABOUT PROJECT</Link>  
-        <Link className="navigation_link" to="/DiscoverNWCStories">DISCOVER NWC STORIES</Link>  
+        <Link className="navigation_link" to="/About">ABOUT PROJECT</Link> 
+        <div></div> 
+        {/*<Link className="navigation_link" to="/DiscoverNWCStories">DISCOVER NWC STORIES</Link>  
         <Link className="navigation_link" to="/WhyTheNWCMatters">WHY THE NWC MATTERS</Link>
         <Link className="navigation_link" to="/MappingNWC">MAPPING THE NWC</Link>
         <Link className="navigation_link" to="/HowToContribute">HOW TO CONTRIBUTE</Link>
-        <img className="navigation_search" src={icon_search}/>
+        <img className="navigation_search" src={icon_search}/>*/}
     </div>
     <div className="navigation_border">
       
