07b87ea6c30d5fdaf1dda806a1c8a0a696e5117a
Peggy Lindner <plindner@uh.edu>
2021-11-21 12:18:33 -0600

    updates for Layout3

--- src/Pages/Essay/Layout3.js
+++ src/Pages/Essay/Layout3.js
@@ -18,6 +18,7 @@ function Layout3() {
         section2: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum interdum odio diam, quis rutrum enim laoreet sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque blandit, est sed porta finibus, velit turpis sodales nisi, eu dapibus risus augue vel dolor. Nam a orci sollicitudin, interdum ligula eu, viverra sem. Etiam vitae mollis nunc, quis pellentesque sem. Vivamus vestibulum sodales dui eget tempor. Fusce eget ultrices justo, at bibendum velit. Duis commodo non ex vel euismod.",
         section3Text: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum interdum odio diam, quis rutrum enim laoreet sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque blandit, est sed porta finibus, velit turpis sodales nisi, eu dapibus risus augue vel dolor. Nam a orci sollicitudin, interdum ligula eu, viverra sem. Etiam vitae mollis nunc, quis pellentesque sem. Vivamus vestibulum sodales dui eget tempor. Fusce eget ultrices justo, at bibendum velit. Duis commodo non ex vel euismod.",
         section3Quote: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum interdum odio diam, quis rutrum enim laoreet sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque blandit, est sed porta finibus, velit turpis sodales nisi, eu dapibus risus augue vel dolor. Nam a orci sollicitudin, interdum ligula eu, viverra sem. Etiam vitae mollis nunc, quis pellentesque sem. Vivamus vestibulum sodales dui eget tempor. Fusce eget ultrices justo, at bibendum velit. Duis commodo non ex vel euismod.",
+        section4: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum interdum odio diam, quis rutrum enim laoreet sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque blandit, est sed porta finibus, velit turpis sodales nisi, eu dapibus risus augue vel dolor. Nam a orci sollicitudin, interdum ligula eu, viverra sem. Etiam vitae mollis nunc, quis pellentesque sem. Vivamus vestibulum sodales dui eget tempor. Fusce eget ultrices justo, at bibendum velit. Duis commodo non ex vel euismod.",
         authorCred: "Written by Jane Doe",
         sources: ["Source A", "Source B",]
     });
@@ -37,7 +38,8 @@ function Layout3() {
                 section2: data.Paragraph2,
                 section3Text: data.Paragraph3,
                 section3Quote: data.PullQuote2,
-                authorCred: data.Author,
+                section4: data.Paragraph4,
+                authorCred: data.AuthorCredit,
                 sources: data.Sources.map(src => src.text),
             })
         });
@@ -49,8 +51,8 @@ function Layout3() {
             <img src={toform} alt=""/>
             <p className="layout3_quote">{pageState.quote}</p>
             <div className="layout3_section1">
-                <p>{pageState.section1_p1}</p>
-                <p>{pageState.section1_p2}</p>
+                <p>{pageState.section1_p1}
+                {pageState.section1_p2}</p>
             </div>
             <ReactMarkdown className="layout3_section2">
                 {pageState.section2}
@@ -59,13 +61,17 @@ function Layout3() {
                 <ReactMarkdown className="layout3_section3Text">{pageState.section3Text}</ReactMarkdown>
                 <p className="layout3_section3Quote">{pageState.section3Quote}</p>
             </p>
-            <p className="layout3_author">
+            <ReactMarkdown className="layout3_section2">
+                {pageState.section4}
+            </ReactMarkdown>
+            <ReactMarkdown className="layout3_author">
                 {pageState.authorCred}
-            </p>
+            </ReactMarkdown>
             <div className="layout3_sources">
                 <h2>SOURCES</h2>
                 {pageState.sources.map(src => <p>{src}</p>)}
             </div>
+
         </div>
     )
 }
