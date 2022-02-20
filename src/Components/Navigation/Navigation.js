17dc14d9719e49c62cd00f7365ce03b3b0bd3d7e
Patrick Kurth <patrickwkurth@gmail.com>
2022-02-20 10:39:19 -0600

    Creation of How to Contribute page
    Created how to contribute page as close to the designing principal given to me.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,16 +6,15 @@ import { Link, useLocation } from 'react-router-dom';
 function Navigation() {
   const location = useLocation();
 
-  function JuicyLink({navname, pathname}) {
+  function JuicyLink({ navname, pathname }) {
     return (
       <Link
         className={
-          `navigation_link ${
-            (
-              location.pathname === pathname
+          `navigation_link ${(
+            location.pathname === pathname
               ? "navigation_link--selected"
               : ""
-            )
+          )
           }`
         }
         to={pathname}
@@ -27,22 +26,23 @@ function Navigation() {
 
   return (
     <div className="navigation_total">
-    <div className="navigation">
+      <div className="navigation">
         <div></div>
-        <JuicyLink navname="Home" pathname="/"/>
-        <JuicyLink navname="About Project" pathname="/About"/>
-        <JuicyLink navname="Why The NWC Matters" pathname="/Why"/>
-        <JuicyLink navname="Discover NWC Stories" pathname="/DiscoverNWCStories"/>
-        <JuicyLink navname="Mapping the NWC" pathname="/MappingNWC"/>
-        <div></div> 
+        <JuicyLink navname="Home" pathname="/" />
+        <JuicyLink navname="About Project" pathname="/About" />
+        <JuicyLink navname="Why The NWC Matters" pathname="/Why" />
+        <JuicyLink navname="Discover NWC Stories" pathname="/DiscoverNWCStories" />
+        <JuicyLink navname="Mapping the NWC" pathname="/MappingNWC" />
+        <div></div>
+        <JuicyLink navname="How To Contribute" pathname="/HowToContribute" />
         {/*
         <Link className="navigation_link" to="/HowToContribute">HOW TO CONTRIBUTE</Link>
         <img className="navigation_search" src={icon_search}/>*/}
-    </div>
-    <div className="navigation_border">
-      
-    </div>
-    </div>
+      </div >
+      <div className="navigation_border">
+
+      </div>
+    </div >
   )
 }
 
