d7489d378e9cd0ce55619205ab8414601a6a822d
Patrick Kurth <patrickwkurth@gmail.com>
2022-02-22 15:56:27 -0600

    IF Conditional for profilepic/alt on DiscInfo

--- src/Pages/Discover/DiscoverInfo.js
+++ src/Pages/Discover/DiscoverInfo.js
@@ -13,6 +13,7 @@ import {
 import "./DiscoverInfo.css"; // This is section 1.
 import ReactMarkdown from 'react-markdown';
 import VARIABLES from '../../config/.env';
+import button from "../../res/imgs/toform.png";
 
 
 function DiscoverInfo() {
@@ -21,12 +22,11 @@ function DiscoverInfo() {
     const { fetchBaseUrl } = VARIABLES;
 
     // this is section 2.
-
     // i predefine here mostly to avoid crashes.
     // for instance, if i have not defined state.career as an array, it does not have the map function.
     // so, in the user interface, if i have {state.career.map(c => c)}, it will throw an error before state.career is populated with an array object.
     // will throw an error along the lines of "undefined has no attribute 'map'".
-    const [ state, setState ] = useState({
+    const [state, setState] = useState({
         bigquote1: '',
         bigquote2: '',
         career: [],
@@ -42,41 +42,56 @@ function DiscoverInfo() {
         state: '',
         usertags: [],
     });
-
     // grab page data from strapi
     useEffect(() => {
         // this pattern is pretty much seen on all data-driven pages
         fetch(`${fetchBaseUrl}/content-discover-stories?_id=${storyId}`)
-        .then(res => res.json())
-        .then(data => {
-            // here i keep the attribute names close to what is served in strapi.
-            // there is still come cleaning I do, as useState does NOT like nested loops.
-            // for example
-            // setState({ nestedEl: { a: 'b' } }); // this will crash things.
-            // this is why the occasional map function is seen in below's assignments.
-            // we want [literal1, literal2, ...], not [{object1}, {object2}, ...] (which is the purpose of the occasional .map)
-            setState({
-                bigquote1: data[0].bigquote1,
-                bigquote2: data[0].bigquote2,
-                career: data[0].career.map(c => c.text),
-                dob: data[0].dob,
-                imgcaption: data[0].imgcaption,
-                maintext: data[0].maintext,
-                name: data[0].name,
-                // this is just getting the url. this pattern is common for media objects, like images and pdf files.
-                // this is also acheived in media2url, which can be found in util.js,
-                // but i think that level of abstraction is unnecessary.
-                // the string literal is my personal preference.
-                profilepic: `${fetchBaseUrl}${data[0].profilepic[0].url}`,
-                profilepic_alt: data[0].profilepic[0].alternativeText,
-                role: data[0].role,
-                rolesAtNwc: data[0].rolesAtNwc.map(r => r.text),
-                sources: data[0].sources.map(s => s.text),
-                state: data[0].state,
-                usertags: data[0].usertags.map(t => t.text),
-            });
-        })
-        .catch(err => console.log(err));
+            .then(res => res.json())
+            .then(data => {
+
+                // IF Conditional statement to ensure data[0].profilepic[0] TypeError resolved
+                // If it exists, we can set the state to that profile pic
+                // Else set it to our "button" picture, this is subject to change
+
+                let profilepic = state.profilepic
+                let profilepic_alt = state.profilepic_alt
+                if (data[0].profilepic[0]) {
+                    profilepic = `${fetchBaseUrl}${data[0].profilepic[0].url}`;
+                    profilepic_alt = data[0].profilepic[0].alternativeText;
+                } else {
+
+                    profilepic = button
+                    profilepic_alt = "No Picture Available"
+                }
+
+                // here i keep the attribute names close to what is served in strapi.
+                // there is still come cleaning I do, as useState does NOT like nested loops.
+                // for example
+                // setState({ nestedEl: { a: 'b' } }); // this will crash things.
+                // this is why the occasional map function is seen in below's assignments.
+                // we want [literal1, literal2, ...], not [{object1}, {object2}, ...] (which is the purpose of the occasional .map)
+                setState({
+                    bigquote1: data[0].bigquote1,
+                    bigquote2: data[0].bigquote2,
+                    career: data[0].career.map(c => c.text),
+                    dob: data[0].dob,
+                    imgcaption: data[0].imgcaption,
+                    maintext: data[0].maintext,
+                    name: data[0].name,
+                    // this is just getting the url. this pattern is common for media objects, like images and pdf files.
+                    // this is also acheived in media2url, which can be found in util.js,
+                    // but i think that level of abstraction is unnecessary.
+                    // the string literal is my personal preference.
+                    profilepic,
+                    profilepic_alt,
+                    role: data[0].role,
+                    rolesAtNwc: data[0].rolesAtNwc.map(r => r.text),
+                    sources: data[0].sources.map(s => s.text),
+                    state: data[0].state,
+                    usertags: data[0].usertags.map(t => t.text),
+                });
+            })
+            .catch(err => console.log(err));
     }, []); // eslint-disable-line
 
     // this is section 3, the UI section.
@@ -116,9 +131,10 @@ function DiscoverInfo() {
     // I was being silly.
     // Abstractions (generally) use Javascript.
     // Braindead HTML pretty much does not.
+
     return (
         <div className="discoverInfo">
-            
+
             {/**BANNER */}
             <div className="discoverInfoBanner">
                 <div className="discoverInfoBanner_left">
@@ -133,7 +149,7 @@ function DiscoverInfo() {
                 {/**BODY_LEFT */}
                 <div className="discoverInfoBody_left">
                     <div className="discoverInfoBody_profile">
-                        <img className="discoverInfoBody_pfp" src={state.profilepic} alt={state.profilepic_alt}/>
+                        <img className="discoverInfoBody_pfp" src={state.profilepic} alt={state.profilepic_alt} />
                         <p className="discoverInfoBody_caption">{state.imgcaption}</p>
 
                         <h3 className="discoverInfoBody_hname">NAME</h3>
